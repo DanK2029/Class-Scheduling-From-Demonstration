c13f48e0c541b04fdc0426a4e886a9b9b2d93326
DanK2029 <dpk2029@gmail.com>
2019-11-04 18:37:52 -0500

    added preferences json, added const file, added soft constraints functions

--- schedule.py
+++ schedule.py
@@ -1,11 +1,36 @@
+from consts import *
 import numpy as np
+import json
 from itertools import product
+from timeline import *
+from softContraints import *
 
 
-timeline = {"4:15": ["c1", "c2", "c3", "EC"], "5:15": ["c1", "c2", "EC"], "6:15": ["c1", "c2", "c3", "c4", "EC"]}
-
-
-def get_schedules(timeline):  # returns list of tuples of all possible schedule combinations
+# returns list of tuples of all possible schedule combinations
+def get_schedules(timeline):
     list_of_schedules = list(product(*timeline.values()))
-    print(list_of_schedules)
+
     return list_of_schedules
+
+
+def main():
+    with open('classes.json', 'r') as class_data_file:
+        classes_json = json.load(class_data_file)
+
+        class_timeline = convertToTimeline(classes_json)
+        all_schedules = get_schedules(class_timeline)
+
+        all_schedules_ex_empty_class = []
+
+        for s in all_schedules:
+            schedules_ex_empty_class = remove_empty_classes(s)
+            if schedules_ex_empty_class:
+                all_schedules_ex_empty_class.append(schedules_ex_empty_class)
+
+        soft_sorted_schedules = sorted(all_schedules_ex_empty_class, key=grade_soft_constraints)
+
+        print(soft_sorted_schedules)
+
+
+if __name__ == '__main__':
+    main()
