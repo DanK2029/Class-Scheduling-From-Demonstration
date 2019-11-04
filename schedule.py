31abec43a468dce075bb275beebf9114faf32181
Admin <admin@lawn-128-61-52-160.lawn.gatech.edu>
2019-11-04 18:37:48 -0500

    new changes to schedule.py inclduing addition of time_converter and analize_schedule functions

--- schedule.py
+++ schedule.py
@@ -1,11 +1,48 @@
 import numpy as np
 from itertools import product
-
-
-timeline = {"4:15": ["c1", "c2", "c3", "EC"], "5:15": ["c1", "c2", "EC"], "6:15": ["c1", "c2", "c3", "c4", "EC"]}
+import ConvertClassesToTimeline as ct
 
 
 def get_schedules(timeline):  # returns list of tuples of all possible schedule combinations
     list_of_schedules = list(product(*timeline.values()))
-    print(list_of_schedules)
     return list_of_schedules
+
+
+def analize_schedule(list_of_schedules, time_constraints):  # list of time constraints
+    time_constraints = time_converter(time_constraints)
+    classesList = []
+    for classTup in list_of_schedules:
+        for i in range(len(classTup)):
+            for time in time_constraints:
+                if (time[0] == classTup[i]['time'][0] or abs(time[0] - classTup[i]['time'][0]) <= 30) and (classTup[i]["days"] == time[1]):
+                    if time[0] <= classTup[i]['time'][0] or time[0] >= classTup[i]['time'][1]:
+                        if classTup[i] not in classesList:
+                            classesList.append(classTup[i])
+    return classesList
+
+
+def time_converter(time_constraints):  # takes in a list of time constraints from user
+    convertedTime_constraints = []
+    for time in time_constraints:
+        if time[0][-2:] == 'am':
+            timeInt = int(time[0][:-2])
+            hr_to_min = timeInt * 60
+            convertedTime_constraints.append(hr_to_min)  # convert to military time
+        elif time[0][-2:] == 'pm':
+            timeInt = int(time[0][:-2])
+            hr_to_min = (timeInt + 12) * 60
+            convertedTime_constraints.append(hr_to_min)
+    return convertedTime_constraints
+
+
+def testcode():
+    class_timeline = ct.classTimeline('testClasses.json')
+    list_of_schedules = get_schedules(class_timeline)
+    time_constraints = [('9am', 'TR'), ('11am', 'MWF'), ('1pm', 'MWF'), ('4pm', 'MWF')]
+
+    classesList = analize_schedule(list_of_schedules, time_constraints)
+    print(classesList)
+
+
+if __name__ == "__main__":
+    testcode()
